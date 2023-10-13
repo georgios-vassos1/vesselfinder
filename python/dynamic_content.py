f4a704cd8c3f4dc2bcbb49b8db36112836440839
georgios.vassos1 <georgios.vassos1@maersk.com>
2023-10-13 11:31:04 +0200

    Correction: dynamic content utilities

--- python/dynamic_content.py
+++ python/dynamic_content.py
@@ -18,15 +18,32 @@ def get_vesselfinder_dynamic(MMSIs: list, headers: dict=None, limit: int=10) -> 
  exceptions = {}
 
  for j, MMSI in enumerate(MMSIs[:limit]):
+
   api_url = f"https://www.vesselfinder.com/api/pub/pcext/v4/{MMSI}?d"
 
   try:
-   dynamic_content = get_dynamic_content(base_url, headers)
+   dynamic_content = scrape_dynamic_table(api_url, headers)
   except Exception as e:
-   print(f"An exception occurred for {IMO}: {e}")
-   exceptions[IMO] = e
+   print(f"An exception occurred for {MMSI}: {e}")
+   exceptions[MMSI] = e
 
   print(f"{j+1} Vessels Completed", end='\r', flush=True)
 
  return dynamic_content, exceptions
 
+
+if __name__ == '__main__':
+
+ headers = {
+  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) \
+                 Chrome/91.0.4472.124 \
+                 Safari/537.11',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+  'Accept-Encoding': 'none',
+  'Accept-Language': 'en-US,en;q=0.8',
+  'Connection': 'keep-alive'
+ }
+
+ MMSI = 228386800
+ print(get_vesselfinder_dynamic([MMSI], headers))
