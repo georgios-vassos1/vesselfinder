3f20b68a9800606085f81a9b452468ee5c11fa07
georgios.vassos1 <georgios.vassos1@maersk.com>
2023-10-20 18:37:06 +0200

    Amendment: month-specific directory records

--- graber.py
+++ graber.py
@@ -7,14 +7,25 @@ import asyncio
 if __name__ == '__main__':
 
  ais_data = asyncio.run(client('Linux'))
- # print(ais_data)
-
  # Serialize the ais_data to a JSON string
  json_data = json.dumps(ais_data, indent=4)
- # Generate a timestamp for the CSV filename
- timestamp = datetime.now().strftime("%Y.%m.%d-%H.%M")
- # Define the path to the CSV file with the timestamp
- json_file_path = f"/app/data/mtraf_{timestamp}.json"
+
+ # Define the base directory where you want to store the files
+ base_dir = "/app/data"
+ # Get the current date
+ current_date = datetime.now()
+ # Create the directory name in the format 'mtraf_%Y%m'
+ dir_name = current_date.strftime("mtraf_%Y%m")
+ # Create the full path for the directory
+ full_dir_path = os.path.join(base_dir, dir_name)
+ # Check if the directory exists, and if not, create it
+ if not os.path.exists(full_dir_path):
+  os.mkdir(full_dir_path)
+
+ # Generate a timestamp for the JSON filename
+ timestamp = current_date.strftime("%Y.%m.%d-%H.%M")
+ # Define the path to the JSON file with the timestamp inside the created directory
+ json_file_path = os.path.join(full_dir_path, f"mtraf_{timestamp}.json")
  # json_file_path = f"~/vessel_track/data/mtraf_{timestamp}.json"
  # Save the JSON data to a file
  with open(json_file_path, "w") as json_file:
