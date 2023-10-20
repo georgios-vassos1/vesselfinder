01affd083b7413a8acef5accb12c5cf3c2291e62
georgios.vassos1 <georgios.vassos1@maersk.com>
2023-10-20 17:51:26 +0200

    Separate client modules to run from local script and by importing the package.

--- vessel_tracker/ais_data_scraper.py
+++ vessel_tracker/ais_data_scraper.py
@@ -15,14 +15,11 @@ async def getXHR(brw_name: str, brw_path: str, url: str = 'https://www.marinetra
 
   # Enable network interception
   await page.route('**/*', lambda route, request: route.continue_())
-
   # Set up an event listener for XHR requests
   xhr_requests = []
   page.on('request', lambda request: xhr_requests.append(request))
-
   # Navigate to the web page
   await page.goto(url)
-
   # Wait for XHR requests to complete (you can customize this part)
   await page.wait_for_timeout(5000)  # Wait for 5 seconds (adjust as needed)
 
@@ -60,33 +57,59 @@ async def get_ais_data(ais: list, headers: dict = None) -> dict:
  async with aiohttp.ClientSession() as session:
   ais_data = await asyncio.gather(*(fetch_data(session, url, headers) for url in ais))
  return ais_data
- # while True:
- #  ais_data = await asyncio.gather(*(fetch_data(session, url, headers) for url in ais))
- #  # Process the ais_data as needed
- #  await asyncio.sleep(300)  # Sleep for 5 minutes (300 seconds)
+
+## Bad practice
+# async def get_ais_data(ais: list, headers: dict = None) -> dict:
+#  while True:
+#   ais_data = await asyncio.gather(*(fetch_data(session, url, headers) for url in ais))
+#   # Process the ais_data as needed
+#   await asyncio.sleep(300)  # Sleep for 5 minutes (300 seconds)
 
 
-async def client():
- headers = {
-  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) \
-                 Chrome/91.0.4472.124 \
-                 Safari/537.11',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-  'Accept-Encoding': 'none',
-  'Accept-Language': 'en-US,en;q=0.8',
-  'Connection': 'keep-alive'
- }
- xhr_requests = await getXHR('chromium', '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
- ais = await get_AIS_XHR(xhr_requests)
- ais_data = await get_ais_data(ais[:2], headers)
+async def client(os_name: str='MacOS'):
+ from vessel_tracker.config import headers_
+ from vessel_tracker.config import chrome_
+
+ xhr_requests = await getXHR(*chrome_[os_name])
+ ais_links    = await get_AIS_XHR(xhr_requests)
+ ais_data     = await get_ais_data(ais_links, headers_)
+
+ return ais_data
+
+
+async def __client(os_name: str='MacOS'):
+ from config import headers_
+ from config import chrome_
+
+ xhr_requests = await getXHR(*chrome_[os_name])
+ ais_links    = await get_AIS_XHR(xhr_requests)
+ ais_data     = await get_ais_data(ais_links, headers_)
+
  return ais_data
 
 
 if __name__ == '__main__':
- ais_data = asyncio.run(client())
- # print(ais_data)
  # loop = asyncio.get_event_loop()
  # loop.run_until_complete(client())
+ import json, os
+ from datetime import datetime
 
+ ais_data = asyncio.run(__client())
+ # print(ais_data)
 
+ # Serialize the ais_data to a JSON string
+ json_data = json.dumps(ais_data, indent=4)
+ # Generate a timestamp for the CSV filename
+ timestamp = datetime.now().strftime("%Y.%m.%d-%H.%M")
+ # Define the path to the CSV file with the timestamp
+ # json_file_path = f"/app/data/mtraf_{timestamp}.json"
+ json_file_path = f"/Users/gva/vessel_track/data/mtraf_{timestamp}.json"
+ # Save the JSON data to a file
+ with open(json_file_path, "w") as json_file:
+  json_file.write(json_data)
+
+ # Check if the file was saved
+ if os.path.exists(json_file_path):
+  print(f"DataFrame saved to {json_file_path}")
+ else:
+  print(f"Failed to save DataFrame to {json_file_path}")
