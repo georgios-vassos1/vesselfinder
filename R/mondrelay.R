b319ed4b776eeb3af1ef675f0b68fc5fec64e292
georgios.vassos1 <georgios.vassos1@maersk.com>
2023-12-04 10:10:00 +0100

    Minor fix

--- R/mondrelay.R
+++ R/mondrelay.R
@@ -42,7 +42,7 @@ get_verification_token <- function(ref_url, ...) {
 
     # Extract the __RequestVerificationToken using regular expressions
     pattern <- 'token="([^"]+)"'
-    token <- stringr::str_match(httr::content(get_, "text"), pattern)[,2L]
+    token   <- stringr::str_match(httr::content(get_, "text"), pattern)[,2L]
 
     return(token)
   }
